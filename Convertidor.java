990a5123186ee828cd5a7635777b25e0e3610f42
diegoeromanmen <125114830+diegoeromanmen@users.noreply.github.com>
2023-02-16 19:56:41 -0600

    Update Convertidor.java

--- Convertidor.java
+++ Convertidor.java
@@ -8,58 +8,77 @@ import java.util.ArrayList;
 
 /**
  *
- * @author normaaliciamendoza
+ * @author normaaliciamendoz
  */
 public class Convertidor {
-    public static String evaluaPost(String[] exp){
+    
+    /**
+     * Evalua la expresión en postfija   
+     * @return el resultado de las opercaiones resultantes.
+     * <ul>
+     * <li> Se recibe un ArrayList con los elementos de la operación en postfija, con signos y numeros por separado</li>
+     * <li> Se utilizan 1 pila auxiliar para efectuar las operaciones. </li>
+     * </ul>
+     */
+    @SuppressWarnings("empty-statement")
+    public static String evaluaPost(ArrayList<String> exp){
         // Se regresará un String, pues se presentará en el JText como un String 
         //De este forma no se castea más veces
-        PilaA pila1= new PilaA();
+        PilaA<String> pila1= new PilaA();
         double a,b, result;
-        String signo, datoF;
+        String signo, datoF="nada";
         int cuantos;
-        cuantos = exp.length;
+        cuantos = exp.size();
          //String[] aux=new String[cuantos];
          //aux=(String[]) exp.toArray();
         //while(!exp.isEmpty()){
+        
+        
+        
         int i=0;
-        while(i<cuantos){
-            pila1.push(exp[i]);
+        while(i<=cuantos ){
+           if(!exp.isEmpty()){
+            pila1.push(exp.get(0));
+            exp.remove(0);
             i++;
+           }
+           
             
-            if(!pila1.peek().equals("+") && !pila1.peek().equals("-")&& !pila1.peek().equals("/")&& !pila1.peek().equals("*")&& !pila1.peek().equals("^")){
-                pila1.push(exp[i]);
-                
+             
+            if(!pila1.peek().equals("+") && !pila1.peek().equals("-") && !pila1.peek().equals("/") && !pila1.peek().equals("*") && !pila1.peek().equals("^")){
+                pila1.push(exp.get(0));
+                exp.remove(0);
+                i++;
             }
             else{
-                if(pila1.peek().equals("+")|| pila1.peek().equals("-") || pila1.peek().equals("/") ||pila1.peek().equals("*") ||pila1.peek().equals("^")){
-                signo=(String)pila1.pop();
-                b=(Double.parseDouble((String)pila1.pop()));
-                a=(Double.parseDouble((String)pila1.pop()));
+                if(pila1.peek().equals("+") || pila1.peek().equals("-") || pila1.peek().equals("/") ||pila1.peek().equals("*") ||pila1.peek().equals("^")){
+                signo=(pila1.pop());
+                b=((Double.parseDouble(pila1.pop())));;
+                a=((Double.parseDouble(pila1.pop())));
                 //Dependiendo del signo se realiza una operación determinada
                 if(signo.equals("+")){
-                    result=(b+a);
-                        pila1.push(result);
+                    result=b+a;
+                        pila1.push(String.valueOf(result));
                 }
                 else
                     if(signo.equals("-")){
                         result=(a-b);
-                        pila1.push(result);
+                        pila1.push(String.valueOf(result));
                     }
                     else 
                         if(signo.equals("/")){
                             result=(a/b);
-                            pila1.push(result);
+                            pila1.push(String.valueOf(result));
                         }
                         else 
                             if(signo.equals("*")){
                                 result=(b*a);
-                                pila1.push(result);
+                                pila1.push(String.valueOf(result));
                             }
                             else
                                  if(signo.equals("^")){
                                      result = Math. pow(a, b);
-                                     pila1.push(result);
+                                     pila1.push(String.valueOf(result));
                                  }
 //                switch(signo){
 //                    case "+" -> {
@@ -86,23 +105,32 @@ public class Convertidor {
 //                }
                 }
                 
+            
             }
             
-                       
             }
-        datoF=(String)pila1.pop();
+        if(i==cuantos){
+                datoF=(String)pila1.pop();
+                
+            }
         return datoF;
-        }
+    }
     
     public static void main(String[] args){
         //ArrayList<String> p=new ArrayList<>();
         String[] p= new String[5];
-        p[0]=("10");
-        p[1]=("2");
-        p[2]=("+");
-        p[3]=("5");
-        p[4]=("*");
-        System.out.println(evaluaPost(p));
+        ArrayList<String> p1=new ArrayList<String>();
+        p1.add("10");
+        p1.add("2");
+        p1.add("+");
+        p1.add("5");
+        p1.add("*");
+//        p[0]=("10");
+//        p[1]=("2");
+//        p[2]=("+");
+//        p[3]=("5");
+//        p[4]=("*");
+        System.out.println(evaluaPost(p1));
     }
     }
 
