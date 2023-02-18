12be91169a72900aca442782a1d051a5f1ff29a7
RetamUwU <lrodr126@itam.mx>
2023-02-18 17:51:07 -0600

    Update Convertidor.java

--- src/Metodos/Convertidor.java
+++ src/Metodos/Convertidor.java
@@ -2,107 +2,93 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Proyecto;
+package Metodos;
 import Pilas.*;
 import java.util.ArrayList;
 
 /**
- *
- * @author normaaliciamendoza
- */
+ * @author Diego Román
+*/
 public class Convertidor {
-    public static String evaluaPost(String[] exp){
-        // Se regresará un String, pues se presentará en el JText como un String 
-        //De este forma no se castea más veces
-        PilaA pila1= new PilaA();
-        double a,b, result;
-        String signo, datoF;
-        int cuantos;
-        cuantos = exp.length;
-         //String[] aux=new String[cuantos];
-         //aux=(String[]) exp.toArray();
-        //while(!exp.isEmpty()){
-        int i=0;
-        while(i<cuantos){
-            pila1.push(exp[i]);
-            i++;
-            
-            if(!pila1.peek().equals("+") && !pila1.peek().equals("-")&& !pila1.peek().equals("/")&& !pila1.peek().equals("*")&& !pila1.peek().equals("^")){
-                pila1.push(exp[i]);
-                
-            }
-            else{
-                if(pila1.peek().equals("+")|| pila1.peek().equals("-") || pila1.peek().equals("/") ||pila1.peek().equals("*") ||pila1.peek().equals("^")){
-                signo=(String)pila1.pop();
-                b=(Double.parseDouble((String)pila1.pop()));
-                a=(Double.parseDouble((String)pila1.pop()));
-                //Dependiendo del signo se realiza una operación determinada
-                if(signo.equals("+")){
-                    result=(b+a);
-                        pila1.push(result);
-                }
-                else
-                    if(signo.equals("-")){
-                        result=(a-b);
-                        pila1.push(result);
-                    }
-                    else 
-                        if(signo.equals("/")){
-                            result=(a/b);
-                            pila1.push(result);
-                        }
-                        else 
-                            if(signo.equals("*")){
-                                result=(b*a);
-                                pila1.push(result);
-                            }
-                            else
-                                 if(signo.equals("^")){
-                                     result = Math. pow(a, b);
-                                     pila1.push(result);
-                                 }
-//                switch(signo){
-//                    case "+" -> {
-//                        result=(b+a);
-//                        pila1.push(result);
-//                        }
-//                    case "-" -> {
-//                        result=(a-b);
-//                        pila1.push(result);
-//                        }
-//                    case "*" -> {
-//                        result=(b*a);
-//                        pila1.push(result);
-//                        }
-//                    case "/" -> {
-//                        result=(a/b);
-//                        pila1.push(result);
-//                        }
-//                    case "^" -> {
-//                        result = Math. pow(a, b);
-//                        pila1.push(result);
-//                        }
-//                        
-//                }
-                }
-                
-            }
-            
-                       
-            }
-        datoF=(String)pila1.pop();
-        return datoF;
-        }
     
-    public static void main(String[] args){
-        //ArrayList<String> p=new ArrayList<>();
-        String[] p= new String[5];
-        p[0]=("10");
-        p[1]=("2");
-        p[2]=("+");
-        p[3]=("5");
-        p[4]=("*");
-        System.out.println(evaluaPost(p));
-    }
+    /**
+     *"EvaluaPost": Evalua la expresión en postfija
+     * @param exp
+     * @return el resultado de las operaciones resultantes.
+     * <ul>
+     * <li> Se recibe un ArrayList ("exp") con los elementos de la operación en
+     * postfija, con signos y números por separado</li>
+     * <li> Se utilizan 1 pila auxiliar para efectuar las operaciones. </li>
+     * </ul>
+     *
+       Explicación:  
+       * -El método se ejecuta dependiendo de la cantidad de elementos en la expresión.
+       * -Se analiza el ArrayList casilla por casilla en busca de operadores. (Siempre detrás de cada operador hay por lo menos dos números)
+       * -Si la casilla que se esta analizando es un número, entonces se introduce a la pila auxiliar (pila1)
+       * -Cada vez que se encuentra un operador se busca qué operación se va a ejecutar y posteriormente se obtienen los dos elementos más 
+       * próximos al operador (respetando su orden).
+       * -Se calcula el resultado y se agrega nuevamente en la pila.
+       * -Si ya se recorrió todo el ArrayList, todas las operaciones correspondientes se realizaron y se devuelve el resultado final (resp).
+     */
+    
+    public static String evaluaPost(ArrayList<String> exp) {
+        String resp = "";
+        if (exp != null && !exp.isEmpty()) {
+            int i = 0, cuantos;
+            cuantos = exp.size();
+            PilaA<Double> pila1 = new PilaA<>();
+            String aux;
+            double a, b, result = 0;
+            while (i < cuantos) {
+                aux = exp.get(i);
+                if (!aux.equals("+") && !aux.equals("-") && !aux.equals("/") && !aux.equals("*") && !aux.equals("^")) {
+                    pila1.push(Double.parseDouble(aux));
+                    i++;
+
+                } else {
+                    //Se asume que en la expresión ya es válida, se validó anteriorimente (siempre hay dos números para operar)
+                    b = pila1.pop();
+                    a = pila1.pop();
+                    //Dependiendo del signo se realiza una operación determinada
+                    if (aux.equals("+")) {
+                        result = a + b;
+                    } else if (aux.equals("-")) {
+                        result = (a - b);
+                    } else if (aux.equals("/")) {// checar que no divida en 0
+                        if (b != 0) {
+                            result = (a / b);
+                        } else {
+                            throw new RuntimeException("No se puede dividir entre 0");
+                        }
+                    } else if (aux.equals("*")) {
+                        result = (b * a);
+                    } else if (aux.equals("^")) {
+                        result = Math.pow(a, b);
+                    }
+                    pila1.push(result);
+                    i++;
+                }
+            }
+            resp = String.valueOf(pila1.pop());
+        }
+        return resp;
+
     }
 
+    public static void main(String[] args) {
+        ArrayList<String> p1 = new ArrayList<String>();
+        p1.add("5");
+        p1.add("6");
+        p1.add("1");
+        p1.add("3");
+        p1.add("+");
+        p1.add("2");
+        p1.add("^");
+        p1.add("*");
+        p1.add("2");
+        p1.add("/");
+        p1.add("+");
+
+        System.out.println(evaluaPost(p1));
+    }
+}
