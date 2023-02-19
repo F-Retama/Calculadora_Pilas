5c46770a91dbad998233f84c85cfab35c930e761
RetamUwU <lrodr126@itam.mx>
2023-02-18 18:46:49 -0600

    Actualización

--- src/Metodos/InfijoAPostfijo.java
+++ src/Metodos/InfijoAPostfijo.java
@@ -1,104 +1,74 @@
-/*
+/*+
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Metodos;
-import Pilas.*;
 import java.util.ArrayList;
+import Pilas.*;
 
 /**
  *
- * @author normaaliciamendoza
+ * @author Alexa
  */
 public class InfijoAPostfijo {
+    //atributos
+    private String cadena;
     
-    public static boolean primerSignoMenorASegundo(String signo1, String signo2){
-        boolean resp=false;
-        if(signo1.equals("*") || signo1.equals("/")){
-            switch(signo2){
-                case "+":
-                    resp=false;
-                    break;
-                case "-":
-                     resp=false;
-                    break;
-                case "*":
-                     resp=true;
-                    break;
-                case "/":
-                     resp=true;
-                    break;
-                case "elevado":
-                     resp=false;
-                    break;
-                    
-        }
-            
-        }
-        else
-            if(signo1.equals("+") || signo1.equals("-")){
-                switch(signo2){
-                case "+":
-                    resp=true;
-                    break;
-                case "-":
-                     resp=true;
-                    break;
-                case "*":
-                     resp=true;
-                    break;
-                case "/":
-                     resp=true;
-                    break;
-                case "elevado":
-                     resp=true;
-                    break;
-            }
+    //constructores
+    public InfijoAPostfijo(){
     }
-        else
-             if(signo1.equals("^")){
-                 resp=false;
-             }
+
+    public InfijoAPostfijo(String cadena) {
+        this.cadena = cadena;
+    }
+    
+    //funcionalidad propia de la clase
+    public ArrayList<Character> convertirPostfijo(String infija){
+        //método un convierte una cadena de infija a postfija.
+        ArrayList<Character> resultado=new ArrayList();
+        PilaADT<Character> elementos=new PilaA<>();
+        int i=0, j=0;
+        while(i<infija.length()){
+            elementos.push(infija.charAt(i));
+            if(determinarOperador(infija.charAt(i))){
+                while(!elementos.isEmpty() && determinarPrioridad(infija.charAt(i))<= elementos.peek())
+                    resultado.add(elementos.pop());
+                elementos.push(infija.charAt(i));
+            }
+            if(infija.charAt(i) == '(')
+                elementos.push(infija.charAt(i));
+            if(infija.charAt(i) == ')'){
+                while(elementos.peek() != '(')
+                    resultado.add(elementos.pop());
+                elementos.pop();
+            }
+            i++;
+        }
+        while(!elementos.isEmpty()){
+            resultado.add(infija.charAt(j));
+            j++;
+        }
+        
+        return resultado;
+    }
+    
+    //métodos estáticos
+    public static int determinarPrioridad(char operador){
+        //método que determina la prioridad de los operadores.
+        int resp=-1;
+        switch(operador){
+            case '^' -> resp=1;
+            case '*' -> resp=2;
+            case '/' -> resp=2;
+            case '+' -> resp=3;
+            case '-' -> resp=3;
+        }
         return resp;
     }
-    public static <T> ArrayList<String> APostfijo (String cadena){
-        ArrayList<String> postfija=new ArrayList<>();
-        PilaA<String> pila1=new PilaA<>();
-        String simbolo;
-        int i=0;
-        
-        while(i<cadena.length()&& !cadena.equals(" ")){//checar esta condicion
-            simbolo=cadena.substring(i, i+1);
-            if(simbolo.equals("(")){
-                pila1.push(simbolo);
-            }
-            else
-                if(simbolo.equals(")")){
-                    while(!pila1.peek().equals("(")){
-                        postfija.add(pila1.pop());
-                        
-                    }
-                    pila1.pop();//solo se quita el parentesis que falta
-                }
-                else //si no es un operador meter el numero a la cadena de postfijo
-               if(!cadena.substring(i, i+1).equals("*") && !cadena.substring(i, i+1).equals("-") && !cadena.substring(i, i+1).equals("/") && !cadena.substring(i, i+1).equals("+") && !cadena.substring(i, i+1).equals("^")){
-                  postfija.add(simbolo);
-               }
-            else
-            if(cadena.substring(i, i+1).equals("*") || cadena.substring(i, i+1).equals("-") || cadena.substring(i, i+1).equals("/") || cadena.substring(i, i+1).equals("+") || cadena.substring(i, i+1).equals("^")){
-                //Falta paso de: llamar a pila vacia con PILA, TOPE Y BAND
-                while(!pila1.isEmpty() && primerSignoMenorASegundo(cadena.substring(i, i+1), pila1.peek()) ){//se verifica que si el operador de la cadena es menor o igual al que esta en el tope de la pila
-                    postfija.add(pila1.pop());
-                    //Falta paso de: llamar a pila vacia con PILA, TOPE Y BAND
-                }
-                pila1.push(simbolo);
-            }
-        }
-        //Falta paso de: llamar a pila vacia con PILA, TOPE Y BAND
-        while(!pila1.isEmpty()){
-            postfija.add(pila1.pop());
-            //Falta paso de: llamar a pila vacia con PILA, TOPE Y BAND
-        }
-        return postfija;
+    public static boolean determinarOperador(char operador){
+        boolean resp=false;
+        if(operador == '+' || operador == '-' || operador == '*' || operador == '/' || operador == '^')
+            resp=true;
+        return resp;
     }
-}
+}//fin clase
