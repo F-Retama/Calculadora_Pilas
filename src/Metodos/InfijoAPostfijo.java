505f73ab4400183d186b3a723f5f7fd4ea408428
Alexa-mpv <amoral98@itam.mx>
2023-02-20 20:30:57 -0600

    Infijo a Postfijo
    Actualización del método que convierte de infijo a postfijo.

--- src/Metodos/InfijoAPostfijo.java
+++ src/Metodos/InfijoAPostfijo.java
@@ -11,51 +11,39 @@ import Pilas.*;
  * @author Alexa
  */
 
-OYEEE, QUÉ VAS A HACER SI LA CADENA EMPIEZA CON '-(' ???
-
-public class InfijoAPostfijo {
-    //atributos
-    private String cadena;
-    
-    //constructores
-    public InfijoAPostfijo(){
-    }
-
-    public InfijoAPostfijo(String cadena) {
-        this.cadena = cadena;
-    }
-    
-    //funcionalidad propia de la clase
-    public ArrayList<Character> convertirPostfijo(String infija){
-        //método un convierte una cadena de infija a postfija.
-        ArrayList<Character> resultado=new ArrayList();
-        PilaADT<Character> elementos=new PilaA<>();
-        int i=0, j=0;
-        while(i<infija.length()){
-            elementos.push(infija.charAt(i));
-            if(determinarOperador(infija.charAt(i))){
-                while(!elementos.isEmpty() && determinarPrioridad(infija.charAt(i))<= elementos.peek())
+public class InfijoAPostfijo {    
+    //métodos estáticos
+    public static ArrayList<String> convertirPostfijo(String infija){
+        //método que convierte una cadena de infija a postfija.
+        ArrayList<String> resultado=new ArrayList();
+        ArrayList<String> cadenaSegmentada= segmenta(infija);
+        PilaADT<String> elementos=new PilaA<>();
+        int i=1, j=0;
+            elementos.push(cadenaSegmentada.get(0));
+        while(i<cadenaSegmentada.size()){
+            if(determinarOperador(cadenaSegmentada.get(i).charAt(i))){
+                while(!elementos.isEmpty() && determinarPrioridad(cadenaSegmentada.get(i).charAt(i)) <= Integer.parseInt(elementos.peek()))
                     resultado.add(elementos.pop());
-                elementos.push(infija.charAt(i));
+                elementos.push(cadenaSegmentada.get(i));
+            } else {
             }
-            if(infija.charAt(i) == '(')
-                elementos.push(infija.charAt(i));
+            if(cadenaSegmentada.get(i).charAt(i) == '(')
+                elementos.push(cadenaSegmentada.get(i));
             if(infija.charAt(i) == ')'){
-                while(elementos.peek() != '(')
+                while(!"(".equals(elementos.peek()))
                     resultado.add(elementos.pop());
                 elementos.pop();
             }
             i++;
         }
         while(!elementos.isEmpty()){
-            resultado.add(infija.charAt(j));
+            resultado.add(cadenaSegmentada.get(j));
             j++;
         }
         
         return resultado;
     }
     
-    //métodos estáticos
     public static int determinarPrioridad(char operador){
         //método que determina la prioridad de los operadores.
         int resp=-1;
@@ -69,9 +57,44 @@ public class InfijoAPostfijo {
         return resp;
     }
     public static boolean determinarOperador(char operador){
+        //método que determina si es un operador.
         boolean resp=false;
         if(operador == '+' || operador == '-' || operador == '*' || operador == '/' || operador == '^')
             resp=true;
         return resp;
+    }
+     public static boolean esDigito(char car){
+         //método que determina si es un dígito.
+        return car == '1' || car == '2' || car == '3' || car == '4' || car == '5' || car == '6' || 
+                car == '7' || car == '8' || car == '9' || car == '0' || car == '.';
+    }
+    
+    public static ArrayList<String> segmenta(String cadena){
+        //método que segmenta la cadena
+        ArrayList <String> resultado = new ArrayList<>();
+        String temp = "";
+        char item;
+        int i=0;
+        while (i<cadena.length()){
+            item = cadena.charAt(i);
+            if (esDigito(item) || item=='-' && temp.isEmpty())
+                temp += item;   
+            else{
+                if (!temp.isBlank())
+                    resultado.add(temp);
+                resultado.add(""+item);
+                temp = "";
+            }
+            i++;
+        }
+        if (!temp.isEmpty())
+            resultado.add(temp);
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        String prueba = "2+3*4";
+        
+        System.out.println(convertirPostfijo(prueba));
     }
 }//fin clase
