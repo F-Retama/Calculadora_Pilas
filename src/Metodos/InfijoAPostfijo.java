af5d50138af74429c79a4a497f8f0630918ac7fd
RetamUwU <lrodr126@itam.mx>
2023-02-21 23:26:47 -0600

    Beta
    Listo para pruebas unitarias

--- src/Metodos/InfijoAPostfijo.java
+++ src/Metodos/InfijoAPostfijo.java
@@ -13,56 +13,25 @@ import Pilas.*;
 
 public class InfijoAPostfijo {    
     //métodos estáticos
-    public static ArrayList<String> convertirPostfijo(String infija){
-        //método que convierte una cadena de infija a postfija.
-        ArrayList<String> resultado=new ArrayList();
-        ArrayList<String> cadenaSegmentada= segmenta(infija);
-        PilaADT<String> elementos=new PilaA<>();
-        int i=1, j=0;
-            elementos.push(cadenaSegmentada.get(0));
-        while(i<cadenaSegmentada.size()){
-            if(determinarOperador(cadenaSegmentada.get(i))){
-                while(!elementos.isEmpty() && determinarPrioridad(cadenaSegmentada.get(i)) <= Integer.parseInt(elementos.peek()))
-                    resultado.add(elementos.pop());
-                elementos.push(cadenaSegmentada.get(i));
-            } else {
-            }
-            if("(".equals(cadenaSegmentada.get(i)))
-                elementos.push(cadenaSegmentada.get(i));
-            if(")".equals(cadenaSegmentada.get(i))){
-                while(!"(".equals(elementos.peek()))
-                    resultado.add(elementos.pop());
-                elementos.pop();
-            }
-            i++;
-        }
-        while(!elementos.isEmpty()){
-            resultado.add(cadenaSegmentada.get(j));
-            j++;
-        }
-        
-        return resultado;
-    }
-    
-    public static int determinarPrioridad(String operador){
+    public static int prioridadOp(String operador){
         //método que determina la prioridad de los operadores.
         int resp=-1;
         switch(operador){
-            case "^" -> resp=1;
-            case "*" -> resp=2;
-            case "/" -> resp=2;
-            case "+" -> resp=3;
-            case "-" -> resp=3;
+            case "+" -> resp = 1;
+            case "-" -> resp = 1;
+            case "*" -> resp = 2;
+            case "/" -> resp = 2;
+            case "^" -> resp = 3;
         }
         return resp;
     }
-    public static boolean determinarOperador(String operador){
-        //método que determina si es un operador.
-        boolean resp=false;
-        if(operador == "+" || operador == "-" || operador == "*" || operador == "/" || operador == "^")
-            resp=true;
-        return resp;
-    }
+//    public static boolean esOperador(String operador){
+//        //método que determina si es un operador.
+//        boolean resp=false;
+//        if(operador == "+" || operador == "-" || operador == "*" || operador == "/" || operador == "^" )
+//            resp=true;
+//        return resp;
+//    }
      public static boolean esDigito(char car){
          //método que determina si es un dígito.
         return car == '1' || car == '2' || car == '3' || car == '4' || car == '5' || car == '6' || 
@@ -77,10 +46,10 @@ public class InfijoAPostfijo {
         int i=0;
         while (i<cadena.length()){
             item = cadena.charAt(i);
-            if (esDigito(item) || item=='-' && temp.isEmpty())
+            if (esDigito(item) || (item=='-' && temp.isEmpty()))
                 temp += item;   
             else{
-                if (!temp.isBlank())
+                if (!temp.isEmpty())
                     resultado.add(temp);
                 resultado.add(""+item);
                 temp = "";
@@ -91,10 +60,61 @@ public class InfijoAPostfijo {
             resultado.add(temp);
         return resultado;
     }
+    public static ArrayList<String> convertirPostfijo(String infija){
+        //método que convierte una cadena de infija a postfija.
+        ArrayList<String> resultado = new ArrayList();
+        ArrayList<String> cadenaSegmentada= segmenta(infija);
+        PilaADT<String> aux = new PilaA<>();
+        int i=0;
+        boolean negInicial = "-".equals(cadenaSegmentada.get(0));
+        if (negInicial){ //caso especial: comienza con "-("
+            resultado.add("-1");
+            i++;
+        }
+        while(i<cadenaSegmentada.size()){
+            String item = cadenaSegmentada.get(i);
+            if(prioridadOp(item) != -1){//es un operador
+                while (!aux.isEmpty() && !"(".equals(aux.peek()) && prioridadOp(item) <= prioridadOp(aux.peek()))
+                    resultado.add(aux.pop());
+                aux.push(item);
+            } 
+            else{
+                switch (item) {
+                    case "(" -> aux.push(item);
+                    case ")" -> {
+                        //vacío los operadores hasta encontrar el paréntesis que abría
+                        while(!"(".equals(aux.peek()))
+                            resultado.add(aux.pop());
+                        aux.pop();
+                        if (negInicial){
+                            resultado.add("*");
+                            negInicial = false;
+                        }
+                    }
+                    default -> resultado.add(item); //es un operando
+                }
+            }
+            i++;
+        }
+        while(!aux.isEmpty()){//vaciar operadores restantes
+            resultado.add(aux.pop());
+        }
+        return resultado;
+    }
+    
 
     public static void main(String[] args) {
-        String prueba = "2+3*4";
         
-        System.out.println(convertirPostfijo(prueba));
+        System.out.println(convertirPostfijo("2+3*4"));
+        System.out.println(convertirPostfijo("2+3+4+5+6"));
+        System.out.println(convertirPostfijo("2*3/4*5/6"));
+        System.out.println(convertirPostfijo("2+3+4*5+6"));
+        System.out.println(convertirPostfijo("2+3*4+5"));
+        System.out.println(convertirPostfijo("((9)+((-9)))"));
+        System.out.println(convertirPostfijo("(-.9*9)"));
+        System.out.println(convertirPostfijo("-1+212*(-8/-94.56+-9)"));
+        System.out.println(convertirPostfijo("-(1+212)*(-8/-94.56+-9)"));
+        System.out.println(convertirPostfijo("-(1+212)+(-8/-94.56+-9)"));
+        
     }
 }//fin clase
